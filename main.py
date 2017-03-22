34b1eac36d20f88b9acf8b194a61babcf10208a1
blackmesataiwan <san82718@gmail.com>
2017-03-22 17:56:10 +0800

    change name

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ bridge_client = bridgeclient()
 
 client = QIoT.setup('./res/resourceinfo.json', '/ssl/')
 
+""" 
+	Receive data of QIoT Suite Lite.
+"""
+
+#Setting Subscribe is use id <QIoT.subscribeofid("ID", client)>
+#It will return topic name
+
 def on_connect(client, userdata, flags, rc):
     global topic_LED
     print("Connected with result code "+str(rc))
@@ -23,6 +30,14 @@ def on_message(client, userdata, msg):
 
 client.on_connect = on_connect
 client.on_message = on_message
+
+
+"""
+	Send sensor's data to QIoT Suite Lite by Resourcetype.
+"""
+
+#It's use "resourcetypename" to sending data.
+#QIoT.sendoftype("resourcetypename", value, client)
 
 
 while True:
